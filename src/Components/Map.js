a5a508821ac1d4c832cde8d7badd339987d5005b
WaneTol72 <wanetols@gmail.com>
2020-07-23 18:25:07 +0300

    Костыль с картой

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-const src = "https://play.nixsv.ru:8123/index.html?worldname=world&mapname=surface&zoom=4&x=-0&y=64&z=0";
+// const src = "https://play.nixsv.ru:8123/index.html?worldname=world&mapname=surface&zoom=4&x=-0&y=64&z=0";
 class Map extends Component {
     render() {
         return (
@@ -7,12 +7,10 @@ class Map extends Component {
                 <div id="map" className="pb-5 bg-dark text-center text-white">
                 <h1 className="pt-2">Карта Classic мира:</h1>
                         <iframe
-                            allowFullScreen={true}
                             title="Map"
                             id="ifr"
                             style={{ height: '100vh', width: '92.5%', border: 'none', borderRadius: 10}}
                             scrolling="no"
-                            src={src}
                         />
                 </div>
             </>
