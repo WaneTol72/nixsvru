79a30339a6b7d39e7476c606ff034b0f4cb75e15
WaneTol72 <wanetols@gmail.com>
2020-07-26 19:07:43 +0300

    Updates

---
+++ precache-manifest.f01ebd6ac477f6e391645fd9ffa12e8c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112b4ca83246013cef30a7d500e9049c",
+    "url": "/nixsvru/index.html"
+  },
+  {
+    "revision": "a641805f7055f718c9cb",
+    "url": "/nixsvru/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d37b1f0065f29c58df54",
+    "url": "/nixsvru/static/css/main.faebfa12.chunk.css"
+  },
+  {
+    "revision": "a641805f7055f718c9cb",
+    "url": "/nixsvru/static/js/2.a2e80072.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/nixsvru/static/js/2.a2e80072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37b1f0065f29c58df54",
+    "url": "/nixsvru/static/js/main.02c05da4.chunk.js"
+  },
+  {
+    "revision": "f4eb731b4fb387743e69",
+    "url": "/nixsvru/static/js/runtime-main.ae1d38b3.js"
+  },
+  {
+    "revision": "3c668a9c5fc018fd6c61be2a61186f35",
+    "url": "/nixsvru/static/media/BARA.3c668a9c.webp"
+  },
+  {
+    "revision": "b0dcd4c7c95e74a96a3f47a9d7f66bf2",
+    "url": "/nixsvru/static/media/DIAMOND.b0dcd4c7.webp"
+  },
+  {
+    "revision": "8a909e590af21e2b5a37ced0a81fa36c",
+    "url": "/nixsvru/static/media/DIS.8a909e59.webp"
+  },
+  {
+    "revision": "5b77ba87ac4684d32573de82a290c089",
+    "url": "/nixsvru/static/media/EMERALD.5b77ba87.webp"
+  },
+  {
+    "revision": "4ea682f7ec862dcd53495f1f9ccdaa22",
+    "url": "/nixsvru/static/media/GOLD.4ea682f7.webp"
+  },
+  {
+    "revision": "3f562def8f04b5f3e889ce2859e643a4",
+    "url": "/nixsvru/static/media/INST.3f562def.webp"
+  },
+  {
+    "revision": "d92d3cd9b6e48fef5450c7a6ab9956bc",
+    "url": "/nixsvru/static/media/IRON.d92d3cd9.webp"
+  },
+  {
+    "revision": "2bd7cacc34cb13883c64aec9b1553ecf",
+    "url": "/nixsvru/static/media/VITEK.2bd7cacc.webp"
+  },
+  {
+    "revision": "e4d0263d40dd05253b32c6c7b16a174b",
+    "url": "/nixsvru/static/media/VK.e4d0263d.webp"
+  },
+  {
+    "revision": "dea9de28dde183abb5a6d191b2f4d42e",
+    "url": "/nixsvru/static/media/WANE.dea9de28.webp"
+  },
+  {
+    "revision": "824438f50b3512775deb8a33cf053695",
+    "url": "/nixsvru/static/media/pic1.824438f5.webp"
+  },
+  {
+    "revision": "4b0645e8f3aa6c31f11c8789d7f9696a",
+    "url": "/nixsvru/static/media/pic2.4b0645e8.webp"
+  },
+  {
+    "revision": "32816f89f72b70dc8ef38bbfe0209808",
+    "url": "/nixsvru/static/media/pickaxe.32816f89.webp"
+  },
+  {
+    "revision": "411a14c754a2eb4f2a7e977934f51032",
+    "url": "/nixsvru/static/media/server.411a14c7.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83458d38353eac352c9e52965867d6c1.js"
+  "/nixsvru/precache-manifest.f01ebd6ac477f6e391645fd9ffa12e8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nixsvru/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
